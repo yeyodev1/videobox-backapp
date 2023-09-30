b8833c966e877c7fea59b4dcc605ec7d706a7297
Diego Reyes <diegorele13@gmail.com>
2023-09-30 16:05:34 -0500

    import model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express';
 import Plans from './plans';
 import Sports from './sports';
-import Leagues from './leagues';
+import Clubs from './clubs';
 import Bets from './bets';
 import Users from './users';
 import Auth from './auth';
@@ -12,7 +12,7 @@ function routerApi(app: Application) {
   app.use('/api', router);
   router.use(Plans);
   router.use(Sports);
-  router.use(Leagues);
+  router.use(Clubs);
   router.use(Bets);
   router.use(Users);
   router.use(Auth);
