2c30314725aece9935e9ec0125e08073358ad724
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-10-23 04:11:21 -0500

    ADD: cronjob

---
+++ src/api/cron.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import syncDriveToGcp from '../tasks/syncDriveAndGcp';
+
+export default async (req: Request, res: Response) => {
+  try {
+    await syncDriveToGcp();
+    console.log('Sincronización con drive y drive');
+    res.status(200).send('Sincronización completada.');
+  } catch (error) {
+    console.error('Error en la sincronización:', error);
+    res.status(500).send('Error en la sincronización.');
+  }
+};
---
+++ src/api/time.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export default async (_req: Request, _res: Response) => {
+  console.log('hello');
+};
